1042267034a70e22e18fcf7e1e6ecaa709330b71
Evans-Chelelgo <57387571+Evans-Chelelgo@users.noreply.github.com>
2019-12-04 14:54:57 +0300

    Add files via upload

--- tri.js
+++ tri.js
@@ -1,11 +1,21 @@
-function tritest(Fside,Sside,lside) {
-var first = document.getElementById('Fside').value;
-var second = document.getElementById('Sside').value;
-var last = document.getElementById('lside').value;
+function tritest(First,Second,last) {
+    var First = document.getElementById('Fside').value;
+    var Second = document.getElementById('Sside').value;
+    var last = document.getElementById('lside').value;
+    if(First==="" || First==="0" || Second==="" || Second==="0" ||last==="" || last==="0"){
+        alert('Enter correct dimensions');
+    }
+    else if(First===Second && Second===last && First===last) {
+        
+alert ("This is an equilateral triangle");
+    }
 
-if(Fside===Sside || Sside===lside || Fside===lside) {
-    alert ("This is an equalateral triangle");
+    else if(First !== Second && First !== last && Second !== last){
+        alert ('This is a scalene triangle');
+    }
+    else {
+    alert ("This is an Isosceles triangle");
 }
-else
-    alert ('This is either an isoceles or a scalene triangle');
 }
+
+
